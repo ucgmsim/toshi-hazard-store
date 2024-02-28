1280759840ace3b32dd64461bd77870e925325e5
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-02-29 11:59:07 +1300

    fix breaking cache put test; WIP;

--- tests/test_query_hazard_caching.py
+++ tests/test_query_hazard_caching.py
@@ -1,12 +1,14 @@
 import itertools
 import random
 import unittest
+from importlib import reload
 
 from moto import mock_dynamodb
 from nzshm_common.location.code_location import CodedLocation
 from nzshm_common.location.location import LOCATIONS_BY_ID
 from pynamodb.models import Model
 
+import toshi_hazard_store.model.openquake_models
 from toshi_hazard_store import model, query
 from toshi_hazard_store.db_adapter import ensure_class_bases_begin_with
 from toshi_hazard_store.model.caching import cache_store
@@ -68,66 +70,55 @@ class TestGetHazardCurvesCached(unittest.TestCase):
 
 
 @mock_dynamodb
-class TestCacheStore(unittest.TestCase):
+def test_cache_put():
+    reload(toshi_hazard_store.model.openquake_models)
 
-    def setUp(self):
-        # the followiung is needed in case an earlier test failed leaving pathed model classes
-        ensure_class_bases_begin_with(
-            namespace=model.__dict__, class_name=str('LocationIndexedModel'), base_class=Model
+    mHAG = toshi_hazard_store.model.openquake_models.HazardAggregation
+    mHAG.create_table(wait=True)
+    conn = cache_store.get_connection(model_class=mHAG)
+
+    n_lvls = 29
+    lvps = list(
+        map(
+            lambda x: toshi_hazard_store.model.openquake_models.LevelValuePairAttribute(lvl=x / 1e3, val=(x / 1e6)),
+            range(1, n_lvls),
         )
-        ensure_class_bases_begin_with(
-            namespace=model.__dict__,
-            class_name=str('HazardAggregation'),  # `str` type differs on Python 2 vs. 3.
-            base_class=model.LocationIndexedModel,
+    )
+    loc = CodedLocation(-43.2, 177.27, 0.001)
+    m = toshi_hazard_store.model.openquake_models.HazardAggregation(
+        values=lvps,
+        vs30=700,
+        agg='mean',
+        imt='PGA',
+        hazard_model_id="HAZ_MODEL_ONE",
+    ).set_location(loc)
+
+    cache_store.put_model(conn, m)
+
+    # now query
+    hash_key = '-43.2~177.3'
+    range_condition = toshi_hazard_store.model.openquake_models.HazardAggregation.sort_key >= '-43.200~177.270:700:PGA'
+    filter_condition = mHAG.vs30.is_in(700) & mHAG.imt.is_in('PGA') & mHAG.hazard_model_id.is_in('HAZ_MODEL_ONE')
+
+    m2 = next(
+        cache_store.get_model(
+            conn,
+            model_class=mHAG,
+            hash_key=hash_key,
+            range_key_condition=range_condition,
+            filter_condition=filter_condition,
         )
-        model.migrate()  # we do this so we get a cache table
-        n_lvls = 29
-        lvps = list(map(lambda x: model.LevelValuePairAttribute(lvl=x / 1e3, val=(x / 1e6)), range(1, n_lvls)))
-        loc = CodedLocation(-43.2, 177.27, 0.001)
-        self.m = model.HazardAggregation(
-            values=lvps,
-            vs30=700,
-            agg='mean',
-            imt='PGA',
-            hazard_model_id="HAZ_MODEL_ONE",
-        ).set_location(loc)
-        # model.drop_tables()
+    )
 
-    # def tearDown(self):
-    #     model.drop_tables()
-    #     folder.cleanup()
-    #     return super(TestCacheStore, self).tearDown()
-
-    def test_cache_put(self):
-        mHAG = model.HazardAggregation
-        mHAG.create_table(wait=True)
-        conn = cache_store.get_connection(model_class=mHAG)
-        cache_store.put_model(conn, self.m)
-
-        # now query
-        hash_key = '-43.2~177.3'
-        range_condition = model.HazardAggregation.sort_key >= '-43.200~177.270:700:PGA'
-        filter_condition = mHAG.vs30.is_in(700) & mHAG.imt.is_in('PGA') & mHAG.hazard_model_id.is_in('HAZ_MODEL_ONE')
-
-        m2 = next(
-            cache_store.get_model(
-                conn,
-                model_class=mHAG,
-                hash_key=hash_key,
-                range_key_condition=range_condition,
-                filter_condition=filter_condition,
-            )
-        )
-
-        assert self.m.sort_key == m2.sort_key
-        # assert self.m.created == m2.created
-        # assert self.m.values == m2.values TODO
-        assert self.m.vs30 == m2.vs30
-        assert self.m.imt == m2.imt
-        assert self.m.nloc_001 == m2.nloc_001
-        assert self.m.agg == m2.agg
-        assert self.m.site_vs30 == m2.site_vs30  # new optional attribute
-        assert self.m.site_vs30 is None
+    assert m.sort_key == m2.sort_key
+    # assert m.created == m2.created
+    # assert m.values == m2.values TODO
+    assert m.vs30 == m2.vs30
+    assert m.imt == m2.imt
+    assert m.nloc_001 == m2.nloc_001
+    assert m.agg == m2.agg
+    assert m.site_vs30 == m2.site_vs30  # new optional attribute
+    assert m.site_vs30 is None
 
 
 @mock_dynamodb
