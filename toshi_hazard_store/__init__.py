86971931f28647921f1c61640e6ba704ad78ef4d
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-03-06 13:58:12 +1300

    add auto-configuration

--- toshi_hazard_store/__init__.py
+++ toshi_hazard_store/__init__.py
@@ -7,4 +7,9 @@ __version__ = '0.8.0'
 
 import toshi_hazard_store.model as model
 import toshi_hazard_store.query.hazard_query as query_v3  # alias for clients using deprecated module name
+from toshi_hazard_store.config import USE_SQLITE_ADAPTER
+from toshi_hazard_store.db_adapter.sqlite import SqliteAdapter
 from toshi_hazard_store.model import configure_adapter
+
+if USE_SQLITE_ADAPTER:
+    configure_adapter(adapter_model=SqliteAdapter)
