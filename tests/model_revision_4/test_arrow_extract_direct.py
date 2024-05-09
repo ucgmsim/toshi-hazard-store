794348881a3ab935266747684b30c049184fa5a7
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-05-09 22:49:25 +1200

    exploring the disagg hdf5 content

--- tests/model_revision_4/test_arrow_extract_direct.py
+++ tests/model_revision_4/test_arrow_extract_direct.py
@@ -18,17 +18,69 @@ except ImportError:
 
 if HAVE_OQ:
     from openquake.calculators.extract import Extractor
-    # from toshi_hazard_store.oq_import import export_rlzs_rev4
 
 from toshi_hazard_store.oq_import.parse_oq_realizations import build_rlz_mapper
-# from toshi_hazard_store.oq_import.oq_manipulate_hdf5 import rewrite_calc_gsims
-# from toshi_hazard_store.model.revision_4 import pyarrow_dataset
 
-def rlzs_to_record_batch_reader(hdf5_file: str):
+def disaggs_to_record_batch_reader(hdf5_file: str) -> pa.RecordBatchReader:
+    """extract disagg statistics from from a 'disaggregation' openquake calc file as a pyarrow batch reader"""
     extractor = Extractor(str(hdf5_file))
+
+    # oqparam contains the job specs, lots of different stuff for disaggs
+    oqparam = json.loads(extractor.get('oqparam').json)
+
+    assert oqparam['calculation_mode'] =='disaggregation', "calculation_mode is not 'disaggregation'"
+
     rlz_map = build_rlz_mapper(extractor)
 
-    oq = json.loads(extractor.get('oqparam').json)
+    # ref https://github.com/gem/oq-engine/blob/75e96a90bbb88cd9ac0bb580a5283341c091b82b/openquake/calculators/extract.py#L1113
+    #
+    # different disagg kinds (from oq['disagg_outputs'])
+    # e.g. ['TRT', 'Mag', 'Dist', 'Mag_Dist', 'TRT_Mag_Dist_Eps']
+    da_trt = extractor.get('disagg?kind=TRT&imt=SA(0.5)&site_id=0&poe_id=0&spec=stats', asdict=True)
+    da_mag = extractor.get('disagg?kind=Mag&imt=SA(0.5)&site_id=0&poe_id=0&spec=stats', asdict=True)
+    da_dist = extractor.get('disagg?kind=Dist&imt=SA(0.5)&site_id=0&poe_id=0&spec=stats', asdict=True)
+    da_mag_dist = extractor.get('disagg?kind=Mag_Dist&imt=SA(0.5)&site_id=0&poe_id=0&spec=stats', asdict=True)
+    da_trt_mag_dist_eps = extractor.get('disagg?kind=TRT_Mag_Dist_Eps&imt=SA(0.5)&site_id=0&poe_id=0&spec=stats', asdict=True)
+
+    '''
+    >>> spec=stats
+    >>> da_trt_mag_dist_eps['array'].shape
+    (1, 24, 17, 16, 1, 1)
+    >>> da_trt_mag_dist_eps.keys()
+    dict_keys(['kind', 'imt', 'site_id', 'poe_id', 'spec', 'trt', 'mag', 'dist', 'eps', 'poe', 'traditional', 'shape_descr', 'extra', 'array'])
+    '''
+
+    '''
+    >>> # STATS
+    >>> da_trt = extractor.get('disagg?kind=TRT&imt=SA(0.5)&site_id=0&poe_id=0&spec=stats', asdict=True)
+    >>> da_trt
+    {'kind': ['TRT'], 'imt': ['SA(0.5)'], 'site_id': [0], 'poe_id': [0], 'spec': ['stats'], 'trt': array([b'Subduction Interface'], dtype='|S20'),
+        'poe': array([9.99412581e-05]), 'traditional': False, 'shape_descr': ['trt', 'imt', 'poe'], 'extra': ['mean'],
+        'array': array([[[9.99466419e-05]]])
+    }
+
+    >>> # RLZS
+    >>> da_trt = extractor.get('disagg?kind=TRT&imt=SA(0.5)&site_id=0&poe_id=0&spec=rlzs', asdict=True)
+    >>> da_trt
+    {'kind': ['TRT'], 'imt': ['SA(0.5)'], 'site_id': [0], 'poe_id': [0], 'spec': ['rlzs'], 'trt':
+        array([b'Subduction Interface'], dtype='|S20'), 'poe': array([9.99412581e-05]), 'traditional': False, 'shape_descr': ['trt', 'imt', 'poe'],
+        'weights': [0.1080000102519989, 0.07200000435113907, 0.09600000828504562, 0.09600000828504562, 0.10000000894069672, 0.07500001043081284, 0.07200000435113907, 0.07200000435113907, 0.08100000768899918, 0.08100000768899918, 0.07200000435113907, 0.07500001043081284],
+        'extra': ['rlz1', 'rlz9', 'rlz10', 'rlz7', 'rlz4', 'rlz3', 'rlz6', 'rlz11', 'rlz0', 'rlz2', 'rlz8', 'rlz5'],
+        'array': array([[[7.27031471e-05, 1.40205725e-04, 6.89674751e-05, 4.83588026e-05,
+             4.67680530e-05, 2.16860247e-04, 2.23101109e-04, 3.09774654e-05,
+             3.68397989e-04, 8.67261109e-06, 6.76580881e-06, 6.21581990e-06]]])}
+    >>>
+    >>>
+    '''
+
+
+def rlzs_to_record_batch_reader(hdf5_file: str) -> pa.RecordBatchReader:
+    """extract realizations from a 'classical' openquake calc file as a pyarrow batch reader"""
+    extractor = Extractor(str(hdf5_file))
+    oqparam = json.loads(extractor.get('oqparam').json)
+    assert oqparam['calculation_mode'] =='classical', "calculation_mode is not 'classical'"
+
+    rlz_map = build_rlz_mapper(extractor)
     #sites = extractor.get('sitecol').to_dframe()
 
     rlzs = extractor.get('hcurves?kind=rlzs', asdict=True)
@@ -40,33 +92,33 @@ def rlzs_to_record_batch_reader(hdf5_file: str):
         site_location_props[props[0]] = coded_location.CodedLocation(lat=props[2], lon=props[1], resolution=0.001).code
 
     #get the IMT props
-    imtls = oq['hazard_imtls']  # dict of imt and the levels used at each imt e.g {'PGA': [0.011. 0.222]}
+    imtls = oqparam['hazard_imtls']  # dict of imt and the levels used at each imt e.g {'PGA': [0.011. 0.222]}
 
     # print(rlz_keys)
     # print('rlzs', rlzs[rlz_keys[0]])
     # print('shape', rlzs[rlz_keys[0]].shape)
     # print()
 
-    def generate_rlz_record_batch(rlz_key:str, rlzs):
-        a = rlzs[rlz_keys[0]] # 3D array for the given rlz_key
-        m,n,r = a.shape
+    def generate_rlz_record_batch(rlz_key:str, rlzs) -> pa.RecordBatch:
 
-        #create the np.arrays for our three table series
-        values = a.reshape(m*n,-1)
-        site_idx = np.repeat(np.arange(m),n)
-        imt_idx = np.tile(np.arange(n), m)
+        a3d = rlzs[rlz_keys[0]] # 3D array for the given rlz_key
+        n_sites, n_imts, n_values = a3d.shape
 
-        # build the series with DictionaryArrays for site and imt
-        # while imtl values are kept in list form
+        #create the np.arrays for our three series
+        values = a3d.reshape(n_sites*n_imts,n_values)
+        site_idx = np.repeat(np.arange(n_sites),n_imts) # 0,0,0,0,0..........3991,3991
+        imt_idx = np.tile(np.arange(n_imts), n_sites)   # 0,1,2,3.....0,1,2,3....26,27
+
+        # build the site and imt series with DictionaryArrays (for effiency)
+        # while imt values are kept in list form
         site_series = pa.DictionaryArray.from_arrays(site_idx, site_location_props.values())
         imt_series = pa.DictionaryArray.from_arrays(imt_idx, imtls.keys())
         values_series = values.tolist()
 
-        # the record batch can be pass to Dataset just like a table
         batch = pa.RecordBatch.from_arrays([site_series, imt_series, values_series], ["nloc_001", "imt", "values"])
         return batch
 
-    #create a schema...
+    # create a schema...
     # TODO add all the fields: nloc_0, gmms_digest etc
     values_type = pa.list_(pa.float32())
     dict_type = pa.dictionary(pa.int32(), pa.string(), True)
@@ -74,7 +126,7 @@ def rlzs_to_record_batch_reader(hdf5_file: str):
 
     print('schema', schema)
 
-    #create an iterator for all the rlz batches
+    # an iterator for all the rlz batches
     def generate_rlz_batches(rlzs, rlz_keys):
         for rlz_key in rlz_keys:
             yield generate_rlz_record_batch(rlz_key, rlzs)
